625683e29d9eeefa1208ab11dbf2400ec1b9e861
Keegan White <keegan28white@gmail.com>
2023-05-04 13:05:35 -0500

    made initial commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject?");
+
+            string subject = Console.ReadLine();
+
+            switch(subject.ToLower()) 
+            {
+                case "math":
+                    Console.WriteLine("I hate math!");
+                    break;
+                case "Science":
+                    Console.WriteLine("I love science!");
+                    break;
+                case "pe":
+                    Console.WriteLine("PE is the best!");
+                    break;
+                case "History":
+                    Console.WriteLine("History is awesome!");
+                    break;
+                case "English":
+                    Console.WriteLine("English is boring.");
+                    break;
+                default:
+                    Console.WriteLine("I don't know that subject.");
+                    break; 
+
+            }
         }
     }
 }
